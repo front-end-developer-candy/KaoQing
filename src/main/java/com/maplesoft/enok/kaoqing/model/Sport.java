58e59e7a7f41635951e4ea06cd985ddc68d23fb2
fengmu456 <416828041@qq.com>
2016-06-27 02:53:56 +0800

    modify

---
+++ src/main/java/com/maplesoft/enok/kaoqing/model/Sport.java
@@ -0,0 +1,34 @@
+package com.maplesoft.enok.kaoqing.model;
+
+public class Sport {
+
+    private String jobNumber;
+
+    private String name;
+
+    private String date;
+
+    public String getJobNumber() {
+        return jobNumber;
+    }
+
+    public void setJobNumber(String jobNumber) {
+        this.jobNumber = jobNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
